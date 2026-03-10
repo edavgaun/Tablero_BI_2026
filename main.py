1b06ed7a13202fd16cf5fc4eba462ee204e9b09f
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2026-03-09 20:51:13 -0600

    Add Streamlit and Pandas imports for GUI

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Sección de importación de módulos
 from Modules.UI.header import show_header
 from Modules.Data.ecobici_service import EcobiciService
+import streamlit as st
+import pandas as pd
 
 # Sección para crear la GUI
 show_header("Mi primera GUI en Streamlit")
