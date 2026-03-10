6a7bd2ff6e90c3efe38c71b1f272b0531e697de5
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2026-03-09 20:47:04 -0600

    Import EcobiciService in main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Sección de importación de módulos
 from Modules.UI.header import show_header
+from Modules.Data.ecobici_service import EcobiciService
 
 # Sección para crear la GUI
 show_header("Mi primera GUI en Streamlit")
+
+st.write(EcobiciService)
